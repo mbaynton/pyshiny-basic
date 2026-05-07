3c2719a198fc8cae973f39da1bb543eb83149acb
Mike Baynton <mike@mbaynton.com>
2026-05-07 15:27:29 -0500

    print user id

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from shiny import App, render, ui
 import os
 import pwd
+import jwt
 
 app_ui = ui.page_fluid(
     ui.input_slider("n", "N slider", 0, 100, 20),
     ui.output_text_verbatim("txt"),
     ui.div(f"Linux user running user code: {pwd.getpwuid(os.getuid()).pw_name}"),
+    ui.output_text_verbatim("user_id"),
 )
 
 
@@ -14,6 +16,18 @@ def server(input, output, session):
     @render.text
     def txt():
         return f"n*2 is going to be {input.n() * 2}"
+
+    @output
+    @render.text
+    def user_id():
+        token = session.http_conn.headers.get("posit-connect-user-session-token")
+        if not token:
+            return "No user session token present."
+        try:
+            claims = jwt.decode(token, options={"verify_signature": False})
+            return f"User ID (sub): {claims.get('sub', '(sub claim not found)')}"
+        except Exception as e:
+            return f"Failed to decode token: {e}"
 
 
 app = App(app_ui, server)
